fd703ec2bf93c00f21dc1075cbd19d53b9f8b286
Pavel Lukačik <Pavel Lukaik@LAPTOP-QNPRC7F3>
2018-12-15 01:29:20 +0100

    38. commit
    mysqlGroupDaoTest (nezbehne zatial), upravene testy, pridane ikonky a
    pozadie (nie vsade)

---
+++ src/test/java/sk/upjs/ics/evidencia_sprostredkovatela/persistent/OrderDaoTest.java
@@ -0,0 +1,14 @@
+package sk.upjs.ics.evidencia_sprostredkovatela.persistent;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrderDaoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/sk/upjs/ics/evidencia_sprostredkovatela/persistent/OrderItemDaoTest.java
@@ -0,0 +1,14 @@
+package sk.upjs.ics.evidencia_sprostredkovatela.persistent;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrderItemDaoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
